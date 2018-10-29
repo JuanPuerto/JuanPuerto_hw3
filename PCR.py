a99da508dfe1caadf82f6d28db768a81bde475e1
JuanPuerto <42391472+JuanPuerto@users.noreply.github.com>
2018-10-28 21:06:24 -0500

    Update PCR.py

--- PCR.py
+++ PCR.py
@@ -2,3 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 cancer = np.genfromtxt('WDBC.dat', delimiter = ',') # Almacena los datos del archivo WDBC.dat en la variable: cancer
+
+def matriz_cov(archivo): # Matriz de covarianza par un archivo
+	dim = np.shape(archivo)[1]	# La dimension de la matriz de covarianza es el numero de columnas del archivo
+	puntos = np.shape(archivo)[0]  # Los puntos de la matriz de covarianza son el numero de filas del archivo
+	covarianza = np.zeros([dim,dim]) # Genera una matriz de ceros con las dimensiones establecidas
+	
+	for i in range(dim):
+		for j in range(dim):
+			promedio_i = np.mean(archivo[:,i]) # Calcula el promedio de los primeros valores
+			promedio_j = np.mean(archivo[:,j]) # Calcula el promedio de los segundos valorea
+			covarianza[i,j] = np.sum((archivo[:,i]-promedio_i)*(archivo[:,j]-promedio_j))/(puntos-1) # Rellena la matriz de covarianza con los valores dados por la ecuacion de covarianza
+	
+	return covarianza 
+	
+covarianza_cancer = matriz_cov(cancer) # Matriz de covarianza para los datos del archivo WDBC.dat
+
+print(covarianza_cancer) # Imprime matriz de covarianza
