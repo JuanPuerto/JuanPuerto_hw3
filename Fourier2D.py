4886b8b109f6f4f25a4f0a7af3e7111c7abb9960
JuanPuerto <42391472+JuanPuerto@users.noreply.github.com>
2018-10-31 20:00:07 -0500

    Update Fourier2D.py

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import fftpack
+from matplotlib.colors import LogNorm
 
 arboles = plt.imread('Arboles.png').astype(float) # Lee la imagen de formato PNG y la transforma en un array de tipo float
 
+arboles_fft = fftpack.fft2(arboles) # Transformada de Fourier
 
+plt.figure()
+plt.imshow(np.abs(arboles_fft) # Grafica transformada de Fourier
+plt.colorbar()
+plt.title('Transformada de Fourier')
+plt.savefig('PuertoJuan_FT2D.pdf')
